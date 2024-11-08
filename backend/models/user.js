58e821cfea414f9df386b68a8eb71a0a48233e68
0PrashantYadav0 <prashantyadav09783@gmail.com>
2024-11-09 01:27:20 +0530

    Add backend models and .gitignore for project setup

---
+++ backend/models/user.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+// Create a Schema for Users
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 5
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
+const User = mongoose.model('User', userSchema);
+export{ User};
